567ab3f3e54643b8a1cd4c58e018887e59694d07
unknown <bysuply30>
2024-05-30 20:37:09 -0500

    nueva contribucion

---
+++ src/main/java/pe/edu/idat/app_demo_spring/controller/HomeController.java
@@ -0,0 +1,10 @@
+package pe.edu.idat.app_demo_spring.controller;
+
+import org.springframework.stereotype.Controller;
+
+public class HomeController {
+
+    public String home() {
+        return "home";
+    }
+}
